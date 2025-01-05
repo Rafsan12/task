ef2de21d7404de5e0aa6ccf72ef1f3e2573649f8
Rafsan12 <rafsan.web.2022@gmail.com>
2025-01-05 22:25:55 +0600

    project complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,7 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      fontFamily: {
-        robotoSlab: '"Roboto Slab", serif;',
-        redHadDisplay: ' "Red Hat Display", serif;',
-      },
-
-      colors: {
-        primary: "#30f557",
-        secondary: "#636662",
-        blackGreen: "#090b08",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
